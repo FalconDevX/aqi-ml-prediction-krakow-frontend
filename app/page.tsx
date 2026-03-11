40b5bf9542f4b49276339e3693edd2ce01815110
SpaceXEngineer <148818352+FalconDevX@users.noreply.github.com>
2026-03-11 02:20:34 +0100

    style: update global styles and enhance map component with district tooltips

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import Header from "@/components/Header";
 
 const Map = dynamic(() => import("@/components/Map"), {
-  ssr: false
+  ssr: false,
 });
 
 export default function Home() {
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <div className="w-[70%] h-[70%]">
+    <div className="h-screen w-full flex flex-col items-center">
+      <Header />
+
+      <div className="w-[70%] h-[70%] mt-10">
         <Map />
       </div>
     </div>
-  );  
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full h-[50px] bg-zinc-900 sticky top-0 z-50 flex items-center">
+      <h1 className="text-2xl font-normal ml-5 text-white">KrakAQI</h1>
+    </div>
+  );
+};
+
+export default Header;
